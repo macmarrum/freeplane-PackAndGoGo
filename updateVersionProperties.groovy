c0060a94a4ceeeff4cb6c3b0b631a121736c9871
macmarrum <7197131+macmarrum@users.noreply.github.com>
2023-06-04 23:46:22 +0200

    add v prefix to version

--- updateVersionProperties.groovy
+++ updateVersionProperties.groovy
@@ -9,9 +9,13 @@ def versionPropertiesFile = me.parent.resolve('version.properties')
 
 def version = LocalDate.now().format('Y.MM.dd')
 
+/** This is how the information is used:
+ * org.freeplane.main.mindmapmode.HttpVersionClient#parseProperties(java.net.URL, org.freeplane.core.util.FreeplaneVersion)
+ * org.freeplane.core.util.FreeplaneVersion#getVersion(java.lang.String) -- Ignores leading 'v' (e.g. "v1.2.6") and accept '.' and ' ' as separator.
+ */
 def template = """\
 version=${version}
-downloadUrl=https://github.com/macmarrum/freeplane-PackAndGoGo/releases/download/v${version}/PackAndGoGo-${version}.addon.mm
+downloadUrl=https://github.com/macmarrum/freeplane-PackAndGoGo/releases/download/v${version}/PackAndGoGo-v${version}.addon.mm
 freeplaneVersionFrom=1.9.1
 """
 println template
